d93ba12ab8f9bf43e7064b654f0dc8b555baff75
Roman Shulga <96079947+romfantast@users.noreply.github.com>
2023-03-26 11:33:21 +0300

    rewrite modals router
    1. add login/register modal router
    2. add style for scrollbar
    3. change z-index in Header.module.css(to look overlay in modal good)
    4. rewrite logic in UserNav to more right way
    5. create new component ModalView to wrap modals

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,57 @@
+// import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from 'pages/HomePage/HomePage';
 import { Layout } from './Layout/Layout';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import ToggleLanguages from './ToggleLanguages';
 import ExampleForToggleLanguages from './ExampleForToggleLanguages';
-// import PublicRoute from './PublicRoute/PublicRoute';
+import PublicRoute from './PublicRoute/PublicRoute';
 // import DynamicsPage from '../pages/DynamicsPage/DynamicsPage'
 
-// import { lazy } from 'react';
+import { ModalView } from './Modal/ModalView';
+import { Modal } from './Modal/Modal';
+import { ModalLogin } from './Modal/ModalLogin';
+import { ModalRegister } from './Modal/ModalRegister';
 
 export const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-      <Route path="/" element={<PrivateRoute />}>
-        <Route path="plan" element={<div>OwnPlanPage</div>} />
-        <Route path="cash-flow" element={<div>ExpensesPage</div>} />
-        <Route path="dynamics" element={<div>OwnPlanPage</div>} />
-        <Route path="expenses" element={<div>Expenses and Categories</div>} />
-        <Route path="*" element={<div>Not Found Page</div>} />
-      </Route>
-    </Routes>
-    <ToggleLanguages />
-    <ExampleForToggleLanguages />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/" element={<PublicRoute />}>
+          <Route
+            path="login"
+            element={
+              <ModalView>
+                <Modal>
+                  <ModalLogin />
+                </Modal>
+              </ModalView>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <ModalView>
+                <Modal>
+                  <ModalRegister />
+                </Modal>
+              </ModalView>
+            }
+          />
+        </Route>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="plan" element={<div>OwnPlanPage</div>} />
+          <Route path="cash-flow" element={<div>ExpensesPage</div>} />
+          <Route path="dynamics" element={<div>OwnPlanPage</div>} />
+          <Route path="expenses" element={<div>Expenses and Categories</div>} />
+          <Route path="*" element={<div>Not Found Page</div>} />
+        </Route>
+      </Routes>
+      <ToggleLanguages />
+      <ExampleForToggleLanguages />
     </>
   );
 };
