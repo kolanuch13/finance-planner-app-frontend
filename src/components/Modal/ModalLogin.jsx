e0a0334302f0c754baa02be4a05249b791ed2689
Roman Shulga <96079947+romfantast@users.noreply.github.com>
2023-03-27 17:36:16 +0300

    add css
    1. add spinner to btn

--- src/components/Modal/ModalLogin.jsx
+++ src/components/Modal/ModalLogin.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './Modals.module.css';
 import { BsEyeSlashFill } from 'react-icons/bs';
 import { BsEye } from 'react-icons/bs';
 import authOperations from 'redux/auth/auth-operations';
+import { RotatingLines } from 'react-loader-spinner';
+import { selectIsLoading } from 'redux/auth/auth-selectors';
 
 export const ModalLogin = () => {
+  const isLoading = useSelector(selectIsLoading);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
@@ -56,6 +59,7 @@ export const ModalLogin = () => {
             name="email"
             onChange={handleChange}
             value={email}
+            disabled={isLoading}
           />
         </label>
         <label className={css.labelWrapper}>
@@ -67,13 +71,24 @@ export const ModalLogin = () => {
             name="password"
             onChange={handleChange}
             value={password}
+            disabled={isLoading}
           />
           <span className={css.eye} onClick={handleToggleEye}>
             {isLookPwd ? <BsEye /> : <BsEyeSlashFill />}
           </span>
         </label>
         <button type="submit" className={css.btn}>
-          Log In
+          {isLoading ? (
+            <RotatingLines
+              strokeColor="#f3f3f3"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="20"
+              visible={true}
+            />
+          ) : (
+            'Log in'
+          )}
         </button>
       </form>
     </div>
