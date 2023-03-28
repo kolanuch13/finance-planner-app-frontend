7e8837d951ee036033674837b63154428ecac2e6
Vitalij Nesvyatipaska <Parkovaja60@gmail.com>
2023-03-28 15:30:45 +0300

    Logout

--- src/components/Header/logout/LogOut.jsx
+++ src/components/Header/logout/LogOut.jsx
@@ -1,14 +1,16 @@
-//import { logOutThunk } from 'redux/auth/authThunk';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../redux/auth/auth-operations';
 import logOutSvg from '../../../images/log-out.svg';
 import css from './LogOut.module.css';
 
 function LogOut() {
+  const dispatch = useDispatch();
   return (
-    <div>
+    <div className={css.logoutBtn}>
       <button
         className={css.logOut}
         type="button"
-        // onClick={() => dispatch(logOutThunk())}
+        onClick={() => dispatch(logout())}
       >
         <p className={css.text}>Log out</p>
         <img src={logOutSvg} alt="logout" className={css.logOutSvg} />
