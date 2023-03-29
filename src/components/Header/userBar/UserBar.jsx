910417a36560cf75a4e3918a4dfbff422ecd43d8
Mykola <kolanuch13@gmail.com>
2023-03-29 09:49:16 +0300

    Add team fuckups.

---
+++ src/components/Header/userBar/UserBar.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import barSvg from '../../../images/bar-graph.svg';
+import css from './UserBar.module.css';
+
+export const UserBar = () => {
+  const userNickName = useSelector(state => state.auth?.user?.username);
+
+  const navigate = useNavigate();
+  const statisticsClick = () => {
+    navigate('/statistics', { replace: true });
+  };
+
+  return (
+    <div className={css.userMenu}>
+      <button type="button" className={css.statistic} onClick={statisticsClick}>
+        <img src={barSvg} alt="statistic" />
+      </button>
+      <p className={css.nickName}>{userNickName}</p>
+    </div>
+  );
+};
