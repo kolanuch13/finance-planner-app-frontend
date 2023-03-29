f41908635c2104d6bdf47682431231bbeaf1ebbb
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2023-03-29 11:24:04 +0300

    sekect

--- src/components/CashflowPage/TransactionDataList/TransactionDataList.jsx
+++ src/components/CashflowPage/TransactionDataList/TransactionDataList.jsx
@@ -3,11 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { userBalance } from '../../../redux/auth/auth-selectors';
 import css from './TransactionDataList.module.css';
 
-export const TransactionDataList = ({setFormData, formData}) => {
+export const TransactionDataList = ({ setFormData, formData }) => {
   // const userBalance = useSelector(balance);
   // const isLoggedIn = useSelector(isLoggedIn);
 
-  const category = ["Other", "Products", "Clothing and footwear"]
+  const category = ['Other', 'Products', 'Clothing and footwear'];
+
+const options = [
+  {value: "other", label: "Other"},
+  {value: "products", label: "Products"},
+  {value: "clothing-and-footwear", label: "Clothing and footwear"},
+]
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -19,50 +25,65 @@ export const TransactionDataList = ({setFormData, formData}) => {
 
   return (
     <>
-      <label for="balance" className={css.label}>
-        From account
-      </label>
-      <input
+      <div className={css.inputWrapper}>
+        <label htmlFor="balance" className={css.label}>
+          From account
+        </label>
+        <input
+        className={css.input}
           id="balance"
           type="text"
           // value={userBalance}
           name="balance"
           readOnly
         />
-
-      <label for="category" className={css.label}>Per category</label>
-      <select
-        id="category"
-        name="category"
-        onChange={handleChange}
-        value={formData.category}
+      </div>
+      <p>Per category</p>
+      <div className={css.inputWrapper}>
+        <label htmlFor="category" className={css.label}>
+          Per category
+        </label>
+        <select
+          className={css.input}
+          id="category"
+          name="category"
+          onChange={handleChange}
+          value={formData.category}
         >
           {category.map((el, i) => (
             <option key={i}>{el}</option>
           ))}
         </select>
-
-      <label for="expenseComment" className={css.label}>
-        Expense comment
-      </label>
-      <input
+      </div>
+      <div className={css.inputWrapper}>
+        <label htmlFor="expenseComment" className={css.label}>
+          Expense comment
+        </label>
+        <input
           id="expenseComment"
           className={css.input}
           type="text"
           value={formData.expenseComment}
           name="expenseComment"
           onChange={handleChange}
+          placeholder="Enter text"
         />
-      <label for="sum" className={css.label}>
-        Sum
-      </label>
-      <input
-        id="sum"
-        type="text"
-        onChange={handleChange}
-        value={formData.sum}
-        name="sum"
-        required />
+      </div>
+      <div className={css.inputWrapper}>
+        <label htmlFor="sum" className={css.label}>
+          Sum
+        </label>
+        <input
+          className={css.input}
+          id="sum"
+          type="text"
+          onChange={handleChange}
+          value={formData.sum}
+          name="sum"
+          required
+          placeholder="Enter sum"
+        />
+      </div>
     </>
   );
 };
