a3748da7f2525ab43f0c2a4b76cd24b96119ddf7
Mykola <kolanuch13@gmail.com>
2023-03-30 09:29:35 +0300

    Some for Selector.

--- src/components/CashflowPage/TransactionDataList/TransactionDataList.jsx
+++ src/components/CashflowPage/TransactionDataList/TransactionDataList.jsx
@@ -3,12 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { balance } from 'redux/auth/auth-selectors';
 import cashflowOperations from 'redux/cashflowPage/cashflowPage-operations';
 import css from './TransactionDataList.module.css';
-
+// import Select from 'react-select'
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import * as icon from 'react-icons/md';
 
 export const TransactionDataList = ({ setFormData, formData }) => {
   const userBalance = useSelector(balance);
   const dispatch = useDispatch();
   const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -21,11 +25,18 @@ export const TransactionDataList = ({ setFormData, formData }) => {
   useEffect(() => {dispatch(cashflowOperations.getCategories())
     .unwrap()
     .then(response => {
-      setCategory(response.data);
-      console.log(category);
-
+      response.data.availableCategoriesEn.map(item => {
+        return setCategory((oldArray) => {
+            return [...oldArray, {
+              value: item.label, 
+              label: item.label, 
+              image: item.image,
+            }]
+        })
     })
-  .catch(error => console.error(error))}, [dispatch])
+      console.log(category[category.length-1]);
+    })
+  .catch(error => console.error(error))}, [])
     
 
   return (
@@ -47,17 +58,57 @@ export const TransactionDataList = ({ setFormData, formData }) => {
         <label htmlFor="category" className={css.label}>
           Per category
         </label>
-        <select
-          className={css.input}
+        <Select
+          variant="standard"
+          labelId="category"
           id="category"
-          name="category"
-          onChange={handleChange}
-          value={formData.category}
+          value={selectedCategory}
+          onChange={e=>setSelectedCategory(e.target.value)}
+          defaultValue={category[category.length-1]}
+          sx={{
+            border: 'none',
+            width: '100%',
+            margin: '0',
+            padding: '0',
+            color: '#F3F3F3',
+            fontFamily: 'Lato',
+            fontSize: '16px',
+            lineHeight: '1.19',
+            backgroundColor: 'none'
+          }}
         >
           {category.map((el, i) => (
-            <option key={i}>{el}</option>
+            <MenuItem key={i} value={el.value}
+            sx={{
+              borderRadius: '16px'
+            }}
+            >
+              <div 
+                className="Container" 
+                dangerouslySetInnerHTML={{__html: `<Md${el.image}`}}
+              ></div>
+              {el.label}
+            </MenuItem>
           ))}
-        </select>
+        </Select>
+        {/* <NativeSelect 
+          id="select"
+          sx={{
+            border: 'none',
+            width: '100%',
+            margin: '0',
+            padding: '0',
+            color: '#F3F3F3',
+            fontFamily: 'Lato',
+            fontSize: '16px',
+            lineHeight: '1.19',
+            backgroundColor: 'none'
+          }}
+        >
+        {category.map((el, i) => (
+            <option key={i} value={el.value}>{el.label}</option>
+          ))}
+        </NativeSelect> */}
       </div>
       <div className={css.inputWrapper}>
         <label htmlFor="expenseComment" className={css.label}>
