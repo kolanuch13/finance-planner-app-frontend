aaa9d8fb415a0475e3efa5ff8cf0390e8d88f1ec
Roman Shulga <96079947+romfantast@users.noreply.github.com>
2023-03-30 16:51:54 +0300

    google auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 // import { lazy } from 'react';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Home } from 'pages/HomePage/HomePage';
 import { Layout } from './Layout/Layout';
+import { useSearchParams } from 'react-router-dom';
 
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 // import ToggleLanguages from './ToggleLanguages';
@@ -17,9 +19,28 @@ import { ModalRegister } from './Modal/ModalRegister';
 import { Verified } from './Modal/Verified';
 import ModalPopUp from './Modal/ModalPopUp';
 import StatisticPage from 'pages/StatisticPage/StatisticPage';
-
+import authOperations from 'redux/auth/auth-operations';
+import { useEffect } from 'react';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log();
+
+  useEffect(() => {
+    const email = searchParams.get('email');
+    const password = searchParams.get('password');
+    if (email && password) {
+      dispatch(authOperations.login({ email, password }))
+        .then(res => {
+          setSearchParams('');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, [dispatch, navigate, searchParams, setSearchParams]);
   return (
     <>
       <Routes>
@@ -32,7 +53,6 @@ export const App = () => {
             <Route path="cashflow" element={<div>ExpensesPage</div>} />
 
             <Route path="dynamics" element={<DynamicsPage />} />
-      
 
             <Route path="statistic" element={<StatisticPage />} />
 
