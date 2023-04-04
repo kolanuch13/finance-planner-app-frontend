7bb66f74f96d08af4dba9e2bcb94ef5813765ccb
Vitalij Nesvyatipaska <Parkovaja60@gmail.com>
2023-04-04 13:32:09 +0300

    Fix bags

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -3,3 +3,4 @@ export const selectToken = state => state.auth.user.token;
 export const balance = state => state.auth.user.balance;
 export const getUser = state => state.auth?.user;
 export const selectIsLoading = state => state.auth.isLoading;
+export const nickName = state => state.auth?.user?.name;
